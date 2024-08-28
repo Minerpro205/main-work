5ec0e30a956def2581f1e37807e20303795421b7
Miner <73215442+Minerpro205@users.noreply.github.com>
2024-08-28 14:53:29 -0400

    Main website upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+let typeOfGreeting= "Hello"
+
+function morning() {
+    alert("It is currently not morning")
+
+
+
+}
+
+function greet(typeOfGreeting, name) {
+    alert(typeOfGreeting + " " + name)
+
+console.log(typeOfGreeting)
+}
+
+// let name = ["Miner", "Mc", 18, "Web development", "Havoc"]
+
+// console.log(name.length)
+
+// let a = 5;
+// let b = 4;
+// let result = 0;
+
+// result = a - b;
+
+// console.log (result)
