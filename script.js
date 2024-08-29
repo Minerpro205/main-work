8188746eceaea6e3f021e8e02e63c31144516100
Miner <minerpro4life@email.com>
2024-08-29 12:14:08 -0400

    Variable testing and remove the salad's sentience

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
+// var num1 = 2
+// var num2 = 5
+// var test1 = num1 % 2
+// var test2 = num2 % 2
+// var result1 = test1 == 0
+// var result2 = test2 == 0
 
+// console.log("Is", num1, "an even number?", result1)
+// console.log("Is", num2, "an even number?", result2)
 
-let typeOfGreeting= "Hello"
+// var added = num1 + num2
+// console.log(added)
 
-function morning() {
-    alert("It is currently not morning")
+// Number variables
+
+// var number = 2
+
+// if(number == 0){console.log("That's 0")}
+// else if(number ==1){console.log("That's 1")}
+// else if(number ==2){console.log("That's 2")}
+// else if(number ==3){console.log("That's 3")}
+// else{console.log("That's not 1-3")}
+
+let namel = "Jackal"
+let nameu = namel.toUpperCase();
+
+switch(nameu){
+    case 'EVA':
+        console.log("That's Eva, she's so cute!")
+        break;
+
+    case 'AYLA':
+        console.log("That's Ayla, she's so chonky!")
+        break;
+
+    case 'JUNI':
+        console.log("That's Juni, she's so tiny!")
+        break;
+
+        case 'JACK':
+        case 'DENNIS':
+        case "NATHAN":
+            console.log("I know", nameu, "from scouts!")
+            break;
+    
+    default: console.log("I don't know who", namel, "is...")
 }
 
-function greet(typeOfGreeting, name) {
-    alert(typeOfGreeting + " " + name)
+// var counter = 100
 
-console.log(typeOfGreeting)
-}
-console.log("hello")
+// while (counter > 0){
+//     console.log(counter);
+//     counter = counter - 1;
+// } console.log("End of counting")
 
-// let name = ["Miner", "Mc", 18, "Web development", "Havoc"]
-
-// console.log(name.length)
-
-// let a = 5;
-// let b = 4;
-// let result = 0;
-
-// result = a - b;
-
-// console.log (result)
+// for (var i = 0; i < 1001; i++){console.log(i)} console.log("That's a lot of numbers")
